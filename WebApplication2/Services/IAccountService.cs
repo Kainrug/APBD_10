8da3ccbb909a4b86be54ffd75d1bcfdf473fbce6
Mateusz Górniak <mateuszgorniak@Mateuszs-MacBook-Air.local>
2024-06-02 12:21:15 +0200

    edit

--- WebApplication2/Services/IAccountService.cs
+++ WebApplication2/Services/IAccountService.cs
@@ -10,7 +10,6 @@ namespace WebApplication2.Services;
 public interface IAccountService
 {
     Task<GetAccountsResponseModel> GetAccountByIdAsync(int id);
-    Task AddProductAsync(AddProductDTO addProduct);
 }
 
 public class AccountService(DatabaseContext context) : IAccountService
@@ -41,48 +40,5 @@ public class AccountService(DatabaseContext context) : IAccountService
             throw new NotFoundException($"Account with {id} does not exist");
         }
         return response;
-    }
-
-    public async Task AddProductAsync(AddProductDTO addProduct)
-    {
-
-        var result = new Product
-        {
-            ProductName = addProduct.Name,
-            ProductWeight = addProduct.Weight,
-            ProductWidth = addProduct.Width,
-            ProductHeight = addProduct.Height,
-            ProductDepth = addProduct.Depth,
-        };
-        
-        
-        if (addProduct.Name is null)
-        {
-            throw new AddProductException("You must type product name");
-        }
-
-        
-        foreach (var productCategory in addProduct.ProductCategories)
-        {
-            var category = await context.Categories.FindAsync(productCategory);
-            if (category == null)
-            {
-                throw new AddProductException($"Category with id {productCategory} does not exist.");
-            }
-        }
-
-        var product = new Product
-        {
-            ProductName = addProduct.Name,
-            ProductWidth = addProduct.Width,
-            ProductHeight = addProduct.Height,
-            ProductDepth = addProduct.Depth,
-            ProductCategories = addProduct.ProductCategories.Select(categoryId => new Product_Category()
-            {
-                CategoryId = categoryId
-            }).ToList()
-        };
-        await context.Products.AddAsync(product);
-        await context.SaveChangesAsync();
     }
 }
